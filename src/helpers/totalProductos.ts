a6f4049e962884a861ac3fd9d186ceb0fe874a8e
Pelusa <agustinlorenzatto@gmail.com>
2025-08-01 16:37:16 -0300

    Acomodando un tipado estricto

--- src/helpers/totalProductos.ts
+++ src/helpers/totalProductos.ts
@@ -1,5 +1,5 @@
-import { Producto } from "@/store/useCarritoStore"
+import { ListaProductos } from "@/store/useCarritoStore"
 
-export const totalProductos = (productos: Producto[]) => {
+export const totalProductos = (productos: ListaProductos[]) => {
     return productos.reduce((acc, prod) => acc + prod.cantidad, 0);
 }
